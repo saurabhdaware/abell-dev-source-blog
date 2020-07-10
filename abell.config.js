af5ce74612273bd5733e4067f745bd6634f409ae
Saurabh Daware <saurabhdaware99@gmail.com>
2020-07-10 13:38:24 +0530

    migrate abell to 0.3.5, abell-source-devto to 0.0.5

--- abell.config.js
+++ abell.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   globalMeta: {
     siteName: "Saurabh's DEV Articles",
+    devMeta: {
+      username: 'saurabhdaware',
+      articleCount: 3
+    },
     name: 'Saurabh Daware',
     twitter: 'saurabhcodes',
     domain: 'https://abell-dev-source-blog.netlify.app'
   },
-  plugins: ['abell-source-devto'],
-  pluginConfig: {
-    devUsername: 'saurabhdaware', 
-    articleCount: 10
-  }
+  plugins: ['abell-source-devto']
 }
